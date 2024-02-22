5b28d68d1a81149b685b953469d6aff0d7c4ba4a
Anthony Watherston <Anthony.Watherston@microsoft.com>
2024-02-22 12:44:34 +1100

    Update message

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ bp = Blueprint("routes", __name__, static_folder="static", template_folder="stat
 UI_TITLE = os.environ.get("UI_TITLE") or "Contoso"
 UI_LOGO = os.environ.get("UI_LOGO")
 UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
-UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Start chatting"
-UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "This chatbot is configured to answer your questions"
+UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Start asking!"
+UI_CHAT_DESCRIPTION = os.environ.get("UI_CHAT_DESCRIPTION") or "This chatbot is configured to answer your questions about Value Based Deliverables."
 UI_FAVICON = os.environ.get("UI_FAVICON") or "/favicon.ico"
 UI_SHOW_SHARE_BUTTON = os.environ.get("UI_SHOW_SHARE_BUTTON", "true").lower() == "true"
 
